26c8da47d1d550b55fe0035ab6a1706931067852
LordSach <sachith.rathnayake.92@gmail.com>
2026-04-07 23:22:30 +0530

    WIP - dummy files were added to form the repo structure demostration

---
+++ rtl/pwm_driver_engine/pwm_driver_engine.sv
@@ -0,0 +1,98 @@
+// ************************************************************************************************************************
+//
+// Copyright (c) 2025 Sachith Rathnayake
+// All Rights Reserved.
+//
+// This software, HDL source code, hardware designs, documentation, and all
+// associated files (collectively, the "Work") are provided for viewing purposes
+// only.
+//
+// No permission is granted to copy, reproduce, modify, merge, publish,
+// distribute, sublicense, create derivative works from, or use the Work, in
+// whole or in part, for any purpose without explicit written permission from
+// the copyright holder.
+//
+// Commercial use requires a separate license agreement. Unauthorized use,
+// reproduction, modification, or distribution of the Work may result in civil
+// and criminal penalties.
+//
+// THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, AND NON-INFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+// BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+// CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE WORK
+// OR THE USE OR OTHER DEALINGS IN THE WORK.
+//
+// For commercial licensing inquiries or support, please contact:
+// Phone:      +94 778 911 111
+// Email:      sachith.rathnayake.92@gmail.com
+// LinkedIn:   https://www.linkedin.com/in/sachith-rathnayake-profile-link/
+// GitHub:     https://github.com/LordSach
+//
+// ************************************************************************************************************************
+//
+// PROJECT      :   Avionics Box
+// PRODUCT      :   PWM Driver Engine
+// FILE         :   pwm_driver_engine.sv
+// AUTHOR       :   Sachith Rathnayake
+// DESCRIPTION  :   PWM Driver Engine design for controlling pwm base actuators 
+//
+// ************************************************************************************************************************
+//
+// REVISIONS:
+//
+//  Date           Developer               Description
+//  -----------    --------------------    -----------
+//  7-APR-2026    Sachith Rathnayake      Creation
+//
+// ************************************************************************************************************************
+
+`timescale 1ns/1ps
+
+module pwm_driver_engine (
+    
+);
+
+    //---------------------------------------------------------------------------------------------------------------------
+    // Global constant headers
+    //---------------------------------------------------------------------------------------------------------------------
+    
+    // constants
+    
+    //---------------------------------------------------------------------------------------------------------------------
+    // parameter definitions
+    //---------------------------------------------------------------------------------------------------------------------
+    
+    // parameters
+    
+    //---------------------------------------------------------------------------------------------------------------------
+    // localparam definitions
+    //---------------------------------------------------------------------------------------------------------------------
+    
+    // localparams
+    
+    //---------------------------------------------------------------------------------------------------------------------
+    // type definitions
+    //---------------------------------------------------------------------------------------------------------------------
+    
+    // typedefs
+    
+    //---------------------------------------------------------------------------------------------------------------------
+    // I/O signals
+    //---------------------------------------------------------------------------------------------------------------------
+    
+    // IO signals
+    
+    //---------------------------------------------------------------------------------------------------------------------
+    // Internal signals
+    //---------------------------------------------------------------------------------------------------------------------
+    
+    // internal signals
+    
+    //---------------------------------------------------------------------------------------------------------------------
+    // Implementation
+    //---------------------------------------------------------------------------------------------------------------------
+    
+    // implementation
+
+endmodule
